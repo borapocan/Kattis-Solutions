c299f90243dc790d2186b8265952c5d38a3dc3ca
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2022-02-23 14:50:15 -0500

    Create sumoftheothers.cpp

---
+++ sumoftheothers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{  
+    string numbers_string;
+  
+    while(getline(cin, numbers_string))
+    {
+      
+        int sum = 0;
+      
+        istringstream iss(numbers_string);
+      
+        for (int n; iss >> n; )
+        {
+          
+            sum += n;
+          
+        }
+      
+        cout << sum/2 << endl;
+      
+    }
+  
+    return 0;
+  
+}
