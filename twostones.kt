90af60a7a37ece1a3a07484293c7fac935e8b052
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2022-03-23 11:00:19 -0400

    Create twostones.kt

---
+++ twostones.kt
@@ -0,0 +1,19 @@
+package com.borapocan.twostones
+
+class TakeTwoStones {
+}
+fun main(args: Array<String>){
+
+    val i = readLine()!!.toInt()
+
+    if (i % 2 == 0) {
+
+        println("Bob")
+
+    }else{
+
+        println("Alice")
+
+    }
+    
+}
