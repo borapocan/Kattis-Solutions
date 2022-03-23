32fcb68945238f01c56a49a827fb6f15f589896e
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2022-03-23 11:13:34 -0400

    Create hissingmicrophone.kt

---
+++ hissingmicrophone.kt
@@ -0,0 +1,19 @@
+package com.borapocan.hissingmicrophone
+
+class HissingMicrophone {}
+
+fun main(args: Array<String>) {
+
+    val input = readLine()!!.toString()
+
+    if (input.contains("ss")) {
+
+        println("hiss")
+
+    }else {
+
+        println("no hiss")
+
+    }
+
+}
