97e9b1d4942c7e97b94052efd010668af6cdb4fc
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2022-03-23 10:55:38 -0400

    Create echoechoecho.cpp

---
+++ echoechoecho.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string str;
+
+    cin >> str;
+
+    for (int i = 0; i < 3; i++)
+    {
+
+        cout << str << " ";
+
+    }
+
+    return 0;
+}
