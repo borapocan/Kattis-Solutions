d731dcc52faefe0f16f9ee326e9d4b6c60f217c9
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2022-03-23 11:03:52 -0400

    Create carrots.kt

---
+++ carrots.kt
@@ -0,0 +1,23 @@
+package com.borapocan.carrots
+
+class SolvingForCarrots {
+}
+
+fun main() {
+
+    val input = readLine()!!.split(' ').map { it.toInt() }
+
+    val n = input[0]
+
+    val p = input[1]
+
+    for (i in 1..n) {
+
+        val contestant = readLine()!!.toString()
+
+    }
+
+    println(p)
+
+
+}
