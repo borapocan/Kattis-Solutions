ce4ee672830aeee8780c00b8ce30076e3ceef861
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2022-04-03 02:17:40 +0300

    Create areal.cpp

---
+++ areal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long n; cin >> n;
+
+    cout << fixed << setprecision(7) << sqrt(n) * 4 << endl;
+
+    return 0;
+}
